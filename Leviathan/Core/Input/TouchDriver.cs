b489a77ffe13d715717ec275d8b415cdf839e84d
Rick_Desktop <rsladest@avans.nl>
2022-02-08 23:56:23 +0100

    [Added] Touch display support

---
+++ Leviathan/Core/Input/TouchDriver.cs
@@ -0,0 +1,183 @@
+﻿using Leviathan.Math;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace Leviathan.Core.Input
+{
+    public enum RegisterTouchFlags
+    {
+        TWF_FINETOUCH = 0x00000001,
+        TWF_WANTPALM = 0x00000002
+    }
+
+    public enum TouchEventFlags
+    {
+        TOUCHEVENTF_MOVE = 0x0001,
+        TOUCHEVENTF_DOWN = 0x0002,
+        TOUCHEVENTF_UP = 0x0004,
+        TOUCHEVENTF_INRANGE = 0x0008,
+        TOUCHEVENTF_PRIMARY = 0x0010,
+        TOUCHEVENTF_NOCOALESCE = 0x0020,
+        TOUCHEVENTF_PEN = 0x0040,
+        TOUCHEVENTF_PALM = 0x0080
+    }
+
+    public struct TOUCH_STRUCT
+    {
+        public Int32 x;
+        public Int32 y;
+        public IntPtr hSource;
+        public UInt32 dwID;
+        public UInt32 dwFlags;
+        public UInt32 dwMask;
+        public UInt32 dwTime;
+        public Int64 dwExtraInfo;
+        public UInt32 cxContact;
+        public UInt32 cyContact;
+    }
+
+    public interface ITouchEventListener
+    {
+        /// <summary>
+        /// The touch event
+        /// </summary>
+        /// <param name="position">The position of the touch point</param>
+        /// <param name="size">The size of the touch point</param>
+        /// <param name="id">The ID of the touch point</param>
+        /// <param name="flags">The flags of the touch point</param>
+        void OnTouchEvent(Vector2f position, Vector2f size, UInt32 id, UInt32 flags);
+    }
+
+    public class TouchDriver : IDisposable
+    {
+        private static readonly uint WM_TOUCH = 0x0240;
+
+        private IntPtr HWnd;
+        private static IntPtr oldproc;
+        private bool bound;
+        private bool registered_touch;
+        private static ITouchEventListener listener;
+
+        delegate IntPtr WndProcDelegate(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+        private WndProcDelegate procfunc;
+
+        private static TouchDriver instance;
+
+        /// <summary>
+        /// A function which gets TouchDriver instance
+        /// </summary>
+        /// <returns>The TouchDriver instance</returns>
+        public static TouchDriver GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new TouchDriver();
+            }
+            return instance;
+        }
+        private TouchDriver() { }
+
+        /// <summary>
+        /// A function which initializes the TouchDriver
+        /// </summary>
+        /// <param name="wnd">A reference to the GLFW window</param>
+        public void Initialise()
+        {
+            this.HWnd = Context.ParentWindow.GetWinApiWindowHandle();
+            LinkToWin32();
+        }
+
+        /// <summary>
+        /// A function which sets an event listener
+        /// </summary>
+        /// <param name="lst">The listener which should be set</param>
+        /// <exception cref="ArgumentNullException">Throws an ArgumentNullException</exception>
+        public void SetEventListener(ITouchEventListener lst)
+        {
+            if (lst == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else
+            {
+                listener = lst;
+            }
+        }
+
+        private unsafe void LinkToWin32()
+        {
+            registered_touch = RegisterTouchWindow(HWnd, (ulong)RegisterTouchFlags.TWF_WANTPALM);
+            if (registered_touch)
+            {
+                procfunc = new WndProcDelegate(WndProc);
+                IntPtr del = Marshal.GetFunctionPointerForDelegate(procfunc);
+                oldproc = SetWindowLongPtr(HWnd, -4, del);
+                bound = true;
+            }
+        }
+
+        private static unsafe IntPtr WndProc(IntPtr hwnd, uint msg, IntPtr wparam, IntPtr lparam)
+        {
+            if (msg == WM_TOUCH)
+            {
+                long touch_event_count = wparam.ToInt64();
+                if (touch_event_count != 0)
+                {
+                    int touch_arr_size = (int)(sizeof(TOUCH_STRUCT) * touch_event_count);
+                    IntPtr touch_arr = Marshal.AllocHGlobal(touch_arr_size);
+                    bool succes = GetTouchInputInfo(lparam, (uint)touch_event_count, touch_arr, (uint)sizeof(TOUCH_STRUCT));
+                    if (succes)
+                    {
+                        TOUCH_STRUCT* ptr = (TOUCH_STRUCT*)touch_arr.ToPointer();
+                        for (int i = 0; i < (int)touch_event_count; i++)
+                        {
+                            TOUCH_STRUCT ts = ptr[i];
+                            Vector2f pos = new Vector2f(ts.x, ts.y);
+                            Vector2f size = new Vector2f(ts.cxContact, ts.cyContact);
+                            listener?.OnTouchEvent(pos, size, ts.dwID, ts.dwFlags);
+                        }
+                        CloseTouchInputHandle(lparam);
+                    }
+                }
+            }
+            return (IntPtr)CallWindowProc(oldproc, hwnd, msg, wparam, lparam);
+        }
+
+        /// <summary>
+        /// A function which disposes of the TouchDriver
+        /// </summary>
+        public void Dispose()
+        {
+            if (registered_touch)
+            {
+                UnregisterTouchWindow(this.HWnd);
+            }
+        }
+
+        #region USERDLL
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool RegisterTouchWindow(IntPtr HWND, ulong flags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool UnregisterTouchWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SetWindowLongPtr(IntPtr HWND, int index, IntPtr func);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool GetTouchInputInfo(IntPtr lParam, uint input_count, IntPtr arr_start, uint arr_size);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool CloseTouchInputHandle(IntPtr hTouchInput);
+
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        static extern long GetLastError();
+        #endregion
+    }
+}
---
+++ Leviathan/Core/Input/TouchPad.cs
@@ -0,0 +1,188 @@
+﻿using Leviathan.Math;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace Leviathan.Core.Input
+{
+
+    /// <summary>
+    /// Structure containing data about a TouchPoint on the screen
+    /// </summary>
+    public struct TouchPoint
+    {
+        /// <summary>
+        /// The TouchPoint X position
+        /// </summary>
+        public Int32 xpos;
+
+        /// <summary>
+        /// The TouchPoint Y position
+        /// </summary>
+        public Int32 ypos;
+
+        /// <summary>
+        /// The TouchPoint contact area width in pixels
+        /// </summary>
+        public UInt32 contact_width;
+
+        /// <summary>
+        /// The TouchPoint contact area height in pixel
+        /// </summary>
+        public UInt32 contact_height;
+
+        /// <summary>
+        /// The TouchPoint virtual id
+        /// </summary>
+        public UInt32 virt_id;
+
+    }
+
+    /// <summary>
+    /// Class that defines behaviour and events for catching and passing TouchEvents
+    /// </summary>
+    public class TouchPanel : ITouchEventListener
+    {
+        /// <summary>
+        /// The touch move function
+        /// </summary>
+        /// <param name="position">The new position</param>
+        /// <param name="delta">The difference between the new and the old position</param>
+        /// <param name="size">The size of the touch points</param>
+        /// <param name="id">The ID of the touch point</param>
+        public delegate void TouchPointMoveFunc(Vector2f position, Vector2f delta, Vector2f size, uint id);
+
+        /// <summary>
+        /// The touch press function
+        /// </summary>
+        /// <param name="position">The position of the touch point</param>
+        /// <param name="size">The size of the touch point</param>
+        /// <param name="id">The ID of the touch point</param>
+        public delegate void TouchPointPressFunc(Vector2f position, Vector2f size, uint id);
+
+        /// <summary>
+        /// The touch release function
+        /// </summary>
+        /// <param name="position">The position of the touch point</param>
+        /// <param name="size">The size of the touch point</param>
+        /// <param name="id">The ID of the touch point</param>
+        public delegate void TouchPointReleaseFunc(Vector2f position, Vector2f size, uint id);
+
+        private Mutex touch_mutex;
+        private Dictionary<UInt32, TouchPoint> captured_points;
+
+        /// <summary>
+        /// Event queue for listening to Touch Move events
+        /// </summary>
+        public event TouchPointMoveFunc TouchMove;
+
+        /// <summary>
+        /// Event queue for listening to Touch Press events
+        /// </summary>
+        public event TouchPointPressFunc TouchPress;
+
+        /// <summary>
+        /// Event queue for listening to Touch Release events
+        /// </summary>
+        public event TouchPointReleaseFunc TouchRelease;
+
+        /// <summary>
+        /// Instantiates a new instance of TouchPanel that hooks into the touch driver event bus
+        /// </summary>
+        /// <param name="drv">The TouchDriver instance</param>
+        public TouchPanel(TouchDriver drv)
+        {
+            touch_mutex = new Mutex();
+            captured_points = new Dictionary<uint, TouchPoint>();
+            captured_points.EnsureCapacity(32);
+            drv.SetEventListener(this);
+        }
+
+        /// <summary>
+        /// Gets a list of all the currently active touch points on the screen
+        /// </summary>
+        /// <returns>A list of all the active touch points on the screen</returns>
+        public List<TouchPoint> GetTouchPoints()
+        {
+            try
+            {
+                touch_mutex.WaitOne();
+                List<TouchPoint> tps = new List<TouchPoint>(captured_points.Values);
+                return tps;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                touch_mutex.ReleaseMutex();
+            }
+        }
+
+        /// <summary>
+        /// Internal interface function for catching touch events from the touch driver
+        /// </summary>
+        /// <param name="position">The position of the touch point</param>
+        /// <param name="size">The size of the touch point in pixels</param>
+        /// <param name="id">The id of the touch point</param>
+        /// <param name="flags">The special flags that identifies the event type</param>
+        public void OnTouchEvent(Vector2f position, Vector2f size, uint id, uint flags)
+        {
+            //Check if the flag for touchevent up is true
+            if ((flags & (uint)TouchEventFlags.TOUCHEVENTF_UP) > 0)
+            {
+                touch_mutex.WaitOne();
+                captured_points.Remove(id);
+                touch_mutex.ReleaseMutex();
+                return;
+            }
+
+            //Check if the flag for touchevent down is true
+            if ((flags & (uint)TouchEventFlags.TOUCHEVENTF_DOWN) > 0)
+            {
+                if (!captured_points.ContainsKey(id))
+                {
+                    TouchPoint tp = new TouchPoint()
+                    {
+                        xpos = (int)position.X,
+                        ypos = (int)position.Y,
+                        contact_width = (uint)size.X,
+                        contact_height = (uint)size.Y,
+                        virt_id = id
+                    };
+                    touch_mutex.WaitOne();
+                    captured_points.Add(id, tp);
+                    touch_mutex.ReleaseMutex();
+                }
+                TouchPress?.Invoke(position, size, id);
+                return;
+            }
+
+            //Check if the flag for touchevent move is true
+            if ((flags & (uint)TouchEventFlags.TOUCHEVENTF_MOVE) > 0)
+            {
+                if (captured_points.ContainsKey(id))
+                {
+                    int delta_x, delta_y;
+                    TouchPoint tp = captured_points[id];
+
+                    delta_x = ((int)position.X) - tp.xpos;
+                    delta_y = ((int)position.Y) - tp.ypos;
+
+
+                    tp.xpos = (int)position.X;
+                    tp.ypos = (int)position.Y;
+                    tp.contact_width = (uint)size.X;
+                    tp.contact_height = (uint)size.Y;
+                    tp.virt_id = id;
+                    captured_points[id] = tp;
+                    TouchMove?.Invoke(position, new Vector2f(delta_x, delta_y), size, id);
+                }
+                return;
+            }
+        }
+    }
+}
